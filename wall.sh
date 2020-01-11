f6c4986dd59d972c1c7bdfb5284d885006a8a704
paperbenni <paperbenni@gmail.com>
2020-01-12 00:33:55 +0100

    add bing wallpaper

--- wall.sh
+++ wall.sh
@@ -11,7 +11,7 @@ mkdir -p $HOME/instantos/wallpapers/default &>/dev/null
 cd $HOME/instantos/wallpapers
 
 randomwallpaper() {
-    if curl google.com; then
+    if curl google.com &> /dev/null; then
         url='https://storage.googleapis.com/chromeos-wallpaper-public'
 
         fetch() {
@@ -24,8 +24,12 @@ randomwallpaper() {
             done
         }
 
-        wget -qO photo.jpg "$url/$(fetch | shuf -n 1)" &>/dev/null
+        wget -qO photo.jpg "$url/$(fetch | shuf -n 1)"
     fi
+}
+
+bingwallpaper() {
+    curl $(curl -s https://bing.biturl.top/ | grep -Eo 'www.bing.com/[^"]*(jpg|png)') >photo.jpg
 }
 
 instantoverlay() {
@@ -46,7 +50,17 @@ fi
 
 genwallpaper() {
     feh --bg-scale default/$(getinstanttheme).png
-    randomwallpaper
+    if [ -n "$1" ]; then
+        randomwallpaper
+    else
+        if $((RANDOM % 2)); then
+            randomwallpaper
+        else
+            bingwallpaper
+        fi
+
+    fi
+
     instantoverlay
     convert photo.jpg -resize 1920x1080^ -extent 1920x1080 wall.png
     convert wall.png -negate invert.png
@@ -59,11 +73,12 @@ genwallpaper() {
 }
 
 if [ -n "$1" ]; then
-    genwallpaper
+    genwallpaper google
 fi
 
 if date +%A | grep -Ei '(Wednesday|Mittwoch)'; then
     if ! [ -e ~/instantos/wallpaper/wednesday ]; then
+
         genwallpaper
         touch ~/instantos/wallpaper/wednesday
     else
